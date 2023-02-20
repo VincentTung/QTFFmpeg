51c0b2f9e9b1b77359a3cfd7f42cccfdad759699
VincentTung <aipincaihuiying18@126.com>
2023-02-20 20:38:44 +0800

    延时播放

--- qt_ffmpeg/mainwindow.cpp
+++ qt_ffmpeg/mainwindow.cpp
@@ -3,14 +3,14 @@
 #include <QDebug>
 extern "C"
 {
-    #include <libavcodec/avcodec.h>
-    #include <libavformat/avformat.h>
-    #include <libswscale/swscale.h>
-    #include <libavdevice/avdevice.h>
-    #include <libavformat/version.h>
-    #include <libavutil/time.h>
-    #include <libavutil/mathematics.h>
-    #include <libavutil/imgutils.h>
+#include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
+#include <libswscale/swscale.h>
+#include <libavdevice/avdevice.h>
+#include <libavformat/version.h>
+#include <libavutil/time.h>
+#include <libavutil/mathematics.h>
+#include <libavutil/imgutils.h>
 }
 #include <QDebug>
 #include "vtdecoder.h"
@@ -19,35 +19,37 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-       //初始化Frames
-     if(! m_frames.init()){
+//    av_log_set_level(AV_LOG_DEBUG) ;
+    //初始化Frames
+    if(! m_frames.init()){
 
-         qDebug()<<"frames init failed";
-         return;
-     }
+        qDebug()<<"frames init failed";
+        return;
+    }
 
-     decoder.setFrames(&m_frames);
+    decoder.setFrames(&m_frames);
 
-     connect(&decoder, &VTDecoder::onNewFrame, this, [this](){
-            if (ui->video_widget->isHidden()) {
-//                ui->loadingWidget->close();
-                ui->video_widget->show();
-            }
-            m_frames.lock();
-            const AVFrame *frame = m_frames.consumeRenderedFrame();
-            qDebug() << "widthxheight:" << frame->width << "x" << frame->height;
-            updateShowSize(QSize(frame->width, frame->height));
-            ui->video_widget->setFrameSize(QSize(frame->width, frame->height));
-            ui->video_widget->updateTextures(frame->data[0], frame->data[1], frame->data[2], frame->linesize[0], frame->linesize[1], frame->linesize[2]);
-            m_frames.unLock();
-        },Qt::QueuedConnection);
-     updateShowSize(size());
-     if(decoder.init()){
+    connect(&decoder, &VTDecoder::onNewFrame, this, [this](){
+        if (ui->video_widget->isHidden()) {
+            //                ui->loadingWidget->close();
+            ui->video_widget->show();
+        }
+        m_frames.lock();
+        const AVFrame *frame = m_frames.consumeRenderedFrame();
+        qDebug() << "widthxheight:" << frame->width << "x" << frame->height;
+        updateShowSize(QSize(frame->width, frame->height));
+        ui->video_widget->setFrameSize(QSize(frame->width, frame->height));
+        ui->video_widget->updateTextures(frame->data[0], frame->data[1], frame->data[2], frame->linesize[0], frame->linesize[1], frame->linesize[2]);
+        m_frames.unLock();
+    },Qt::QueuedConnection);
+    updateShowSize(size());
+    if(decoder.init()){
         qDebug()<<"decoder init success";
-        const char* path = "D:/1989.mp4";
+        const char* path = "D:/1988.mp4";
         decoder.setFilePath(path);
         decoder.startDecode();
-     }
+    }
+
 
 
 
--- qt_ffmpeg/vtdecoder.cpp
+++ qt_ffmpeg/vtdecoder.cpp
@@ -3,15 +3,24 @@
 #include <QImage>
 extern "C"
 {
-    #include <libavcodec/avcodec.h>
-    #include <libavformat/avformat.h>
-    #include <libswscale/swscale.h>
-    #include <libavdevice/avdevice.h>
-    #include <libavformat/version.h>
-    #include <libavutil/time.h>
-    #include <libavutil/mathematics.h>
-    #include <libavutil/imgutils.h>
+#include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
+#include <libswscale/swscale.h>
+#include <libavdevice/avdevice.h>
+#include <libavformat/version.h>
+#include <libavutil/time.h>
+#include <libavutil/mathematics.h>
+#include <libavutil/imgutils.h>
 }
+
+
+
+void logErrorMsg(const char* tag,int ret){
+    char errorbuf[255] = { 0 };
+    av_strerror(ret, errorbuf, 254);
+    qCritical("%s: retcode:%d,errorMsg: %s",tag,ret,errorbuf);
+}
+
 VTDecoder::VTDecoder()
 {
 
@@ -21,8 +30,8 @@ void VTDecoder::startDecode(){
 }
 void VTDecoder::run(){
 
-    AVFormatContext* formatContext = avformat_alloc_context();
 
+    AVFormatContext* formatContext = avformat_alloc_context();
     int res = avformat_open_input(&formatContext,filePath,nullptr,nullptr);
     if(res == 0){
         qDebug()<<"open file success";
@@ -40,11 +49,11 @@ void VTDecoder::run(){
     int videoType = -1;
     for(int i = 0 ; i <formatContext->nb_streams;i++){
 
-       AVStream* stream = formatContext->streams[i];
-       if(stream->codecpar->codec_type == AVMEDIA_TYPE_VIDEO){
-           videoType = i;
-           break;
-       }
+        AVStream* stream = formatContext->streams[i];
+        if(stream->codecpar->codec_type == AVMEDIA_TYPE_VIDEO){
+            videoType = i;
+            break;
+        }
     }
 
     if(videoType == -1){
@@ -54,9 +63,10 @@ void VTDecoder::run(){
         qDebug()<<"有视频流:"<<videoType;
     }
     //查找解码器
-     AVStream* stream = formatContext->streams[videoType];
+    AVStream* stream = formatContext->streams[videoType];
     AVCodecParameters* codecParameter = stream->codecpar;
     const AVCodec* decoder = avcodec_find_decoder(codecParameter->codec_id);
+
 
 
     if(decoder != nullptr){
@@ -69,22 +79,22 @@ void VTDecoder::run(){
     AVCodecContext* codec = avcodec_alloc_context3(decoder);
     //少了这句话
     if(avcodec_parameters_to_context(codec, stream->codecpar)<0){
-         qDebug()<<"avcodec_parameters_to_context failed";
-         return ;
-     }
+        qDebug()<<"avcodec_parameters_to_context failed";
+        return ;
+    }
     //打开解码器
     res = avcodec_open2(codec,decoder,nullptr);
 
     if(res ==0){
         qDebug()<< "open codec sucess";
-
     }else{
         qDebug()<< "open codec failed";
         return;
     }
-\
+    \
     int width =codecParameter->width;
     int height = codecParameter->height;
+
 
     int bit_reate = codecParameter->bit_rate;
     int codec_type = codecParameter->codec_type;
@@ -95,61 +105,89 @@ void VTDecoder::run(){
     int video_delay = codecParameter->video_delay;
     int level = codecParameter->level;
     int profile = codecParameter->profile;
+
     AVRational sample_aspect_ratio = codecParameter->sample_aspect_ratio;
     qDebug()<<"width:"<<width
            <<" height:"<<height
           <<" bitRate:"<<bit_reate
          <<" codecType:"<<codec_type
-           <<" format:"<<formate
-          <<" frameSize:"<<frame_size
-         <<" sampleRate:"<<sample_rate
-        <<" videoDelay:"<<video_delay
-       <<" level:"<<level
-      <<" profile:"<<profile
-     <<" aspectRation:(num:"<<sample_aspect_ratio.num<<",den:"<<sample_aspect_ratio.den<<")";
-   av_dump_format(formatContext, 0, filePath, false);
+        <<" format:"<<formate
+       <<" frameSize:"<<frame_size
+      <<" sampleRate:"<<sample_rate
+     <<" videoDelay:"<<video_delay
+    <<" level:"<<level
+    <<" profile:"<<profile
+    <<" aspectRation:(num:"<<sample_aspect_ratio.num<<",den:"<<sample_aspect_ratio.den<<")";
+    av_dump_format(formatContext, 0, filePath, false);
+
+    qDebug()<<"avg_frame_rate:num->"<<stream->avg_frame_rate.num<<"__den:"<<stream->avg_frame_rate.den;
+    //计算fps 帧率
+    float frameRate = stream->avg_frame_rate.num/stream->avg_frame_rate.den;
+    qDebug()<<"frameRate:"<<frameRate;
+
     AVPacket* packet = av_packet_alloc();
-//    AVFrame* decodingFrame = av_frame_alloc();
+    packet->data = NULL;
+    packet->size = 0;
+
     if(m_frames == nullptr){
-
         qDebug()<<"m_frames is null";
-
         return ;
     }
-    AVFrame* decodingFrame = m_frames->decodingFrame();
 
-    while(av_read_frame(formatContext,packet) >=0){
 
-           if (packet->stream_index == videoType) {
+    while(!av_read_frame(formatContext,packet)){
 
-               int ret = avcodec_send_packet(codec,packet);
-               if(ret < 0||ret == AVERROR(EAGAIN) || ret == AVERROR_EOF){
-                   qDebug()<<"avcodec_send_packet:"<<ret;
-                   char errorbuf[255] = { 0 };
-                             av_strerror(ret, errorbuf, 254);
-                             qCritical("Could not send video packet: %s", errorbuf);
-                   break;
-               }
-               while(ret >= 0){
-                   ret = avcodec_receive_frame(codec,decodingFrame);
-                   if(ret == AVERROR(EAGAIN) || ret == AVERROR_EOF){
-                       qDebug()<<"receive break";
-                       break;
-                   }else if(ret ==0){
+        if (packet->stream_index == videoType) {
 
-                       qDebug()<<"avframe after dcode:"<<decodingFrame->width<<"x"<<decodingFrame->height;
-                       pushFrame();
-                   }
 
-//                   //解码后的数据
-//                   decodingFrame->data;
-                   qDebug()<<"frame:"<<codec->frame_number;
-               }
+            int ret = avcodec_send_packet(codec,packet);
+            if(ret < 0||ret == AVERROR(EAGAIN) || ret == AVERROR_EOF){
+                logErrorMsg("avcodec_send_packet error:", ret);
+                goto runQuit;
+                break;
+            }
 
-           }
-           av_packet_unref(packet);
 
-       }
+            AVFrame* decodingFrame = m_frames->decodingFrame();
+            if (decodingFrame) {
+                ret = avcodec_receive_frame(codec, decodingFrame);
+                if(!ret){
+                    qDebug()<<"avframe after dcode:"<<decodingFrame->width<<"x"<<decodingFrame->height;
+                    pushFrame();
+
+                }else{
+                    av_packet_unref(packet);
+                    logErrorMsg("avcodec_receive_frame other error:", ret);
+                    goto runQuit;
+                }
+
+                //解码后的数据
+                // decodingFrame->data;
+                qDebug()<<"frame:"<<codec->frame_number;
+                usleep(1000*1000/frameRate);
+                av_packet_unref(packet);
+
+            }
+
+        }
+
+    }
+
+runQuit:
+    qDebug()<<"quiting";
+    if (formatContext) {
+        avformat_close_input(&formatContext);
+    }
+    if (formatContext) {
+        avformat_free_context(formatContext);
+    }
+    if (codec ) {
+        avcodec_close(codec);
+    }
+    if (codec) {
+        avcodec_free_context(&codec);
+    }
+
 }
 void VTDecoder::setFrames(Frames* frames){
     this->m_frames = frames;
@@ -157,13 +195,13 @@ void VTDecoder::setFrames(Frames* frames){
 
 bool VTDecoder::init(){
 
-    #if LIBAVCODEC_VERSION_INT < AV_VERSION_INT(58, 9, 100)
-        av_register_all();
-    #endif
-        if (avformat_network_init()) {
-            return false;
-        }
-        return true;
+#if LIBAVCODEC_VERSION_INT < AV_VERSION_INT(58, 9, 100)
+    av_register_all();
+#endif
+    if (avformat_network_init()) {
+        return false;
+    }
+    return true;
 
 
 }
@@ -181,5 +219,6 @@ void VTDecoder::pushFrame()
         // the previous newFrame will consume this frame
         return;
     }
+    qDebug()<<"push frame";
     emit onNewFrame();
 }
